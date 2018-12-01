7c512b363ce48c381960f266b106f656210f0f59
Laptop Computer <plyrek@immergenstudios.com>
2018-12-01 15:21:46 -0600

    End of Section 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +11,19 @@ export class RegisterComponent implements OnInit {
   @Output()cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
-  register(){
+  register() {
     // tslint:disable-next-line:max-line-length
-    this.authService.register(this.model).subscribe(next => {console.log('Registration sucessful'); }, error => {console.log(error); });
+    this.authService.register(this.model).subscribe(next => {this.alertify.success('Registration sucessful'); }, error => {this.alertify.error(error); });
       // console.log(this.model);
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Canceled');
   }
 
 }
